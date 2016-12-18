ac2c16f96eea43c39d74e1b116cdec37d6fa437d
JoseCedeno21 <jose.gabriell.21@gmail.com>
2016-12-18 05:15:36 -0500

    ingreso y modificaciones

---
+++ js/ingreso.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	if(screen.width<=800){
+    	console.log("si");
+    	$("#list").append('<li><a class="page-scroll" href="pages/ingreso/ingreso.html">Ingresar</a></li>');
+	}
+})
