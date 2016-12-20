6be1470d68ddc6db93f269a1aa36f362498120b8
Lucrecia Vintimilla <lucrecia.vintimilla@hotmail.com>
2016-12-19 21:55:57 -0500

    informacion del json modificado: sandbox

--- js/datosPerfiles.js
+++ js/datosPerfiles.js
@@ -1,1 +1,24 @@
-datosPerfiles.js
+$(document).ready(function() {
+    var nombre = $("#nombre");
+    var correo = $("#correo");
+    var tipo=$("#tipo");
+    var fechaNac = $("#fechaNac");
+    var numResuelto = $("#numResuelto");
+    var nivel = $("#nivel");
+    var imagen = $("#imagen");
+    $.getJSON("../../../../data/personal/perfiles.json", function(data) {
+        var nombreData = data.nombre;
+        var correoData = data.correo;
+        var tipo = data.tipo;
+        var fechaNacData = data.fechaNac;
+        var numResueltoData = data.numEjerciciosResueltos;
+        var nivelData = data.nivel;
+        var imagenData = data.imagen;
+        nombre.text(nombreData);
+        correo.text(correoData);
+        fechaNac.text(fechaNacData);
+        numResuelto.text(numResueltoData);
+        nivel.text(nivelData);
+        imagen.attr('src', imagenData);
+    });
+});
