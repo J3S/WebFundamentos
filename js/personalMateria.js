094dbf25a72babe12543241bb7ade1723234818b
julio9319 <juliohbk_93@hotmail.es>
2016-12-21 05:19:21 -0500

    Cambios informacion actualizada, tanto en profesores, coordinador, ayudantes

--- js/personalMateria.js
+++ js/personalMateria.js
@@ -5,23 +5,20 @@ var url = document.location.toString();
 if (url.match('#')) {
 	$('.nav-tabs a[href="#'+url.split('#')[1]+'"]').tab('show') ;
 	var x = url.substring(url.lastIndexOf('#')+1);
-	console.log(x);
-	if (url.split('#')[1] == coordinador){
+	
+	if (x === 'coordinador'){
+		console.log(x);
 		$.getJSON("../../data/personal/coordinador.json", function(items){
 				items.coordinador.forEach(function(item){
 					$("#coordinador-materia").append(
 						
-						'<div id="ayudante" class="col-lg-4 col-md-6 col-sm-12 col-xs-12 text-center"><img class="img-circle img-responsive img-center" src="../../img/PersonalMateria/'+item.imagen+'"></img><h3>'+item.nombre+'</h3><p>Correo:'+item.correo+'</p><p>Oficina:'+item.oficina+'</p></div>'
+						'<div id="coordinador" class="col-lg-4 col-md-6 col-sm-12 col-xs-12 text-center"><img class="img-circle img-responsive img-center" src="../../img/PersonalMateria/'+item.imagen+'"></img><h3>'+item.nombre+'</h3><p>Correo:'+item.correo+'</p><p>Oficina:'+item.oficina+'</p></div>'
 						
 					);
 				});
 			})
 	}
 }
-$('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
-	window.location.hash = e.target.hash;
-});
-
 
 
 
@@ -69,20 +66,7 @@ $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
 			})
 		}
     });
-    $('.nav-tabs a[href="#coordinador"]').click(function(){
-        $(this).tab('show');
-        if($('#coordinador-materia').html() == "") {
-	        $.getJSON("../../data/personal/coordinador.json", function(items){
-				items.coordinador.forEach(function(item){
-					$("#coordinador-materia").append(
-						
-						'<div id="ayudante" class="col-lg-4 col-md-6 col-sm-12 col-xs-12 text-center"><img class="img-circle img-responsive img-center" src="../../img/PersonalMateria/'+item.imagen+'"></img><h3>'+item.nombre+'</h3><p>Correo:'+item.correo+'</p><p>Oficina:'+item.oficina+'</p></div>'
-						
-					);
-				});
-			})
-		}
-    });
+    
 
 });
 
@@ -90,53 +74,3 @@ $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
 
 
 
-function datos(){
-
-	if($(".page-header").text() == "Ayudantes de Clases"){
-		$.getJSON("../../data/personal/ayudantesClases.json", function(items){
-			items.ayudantes.forEach(function(item){
-				$(".container .row").append(
-					
-					'<div id="ayudante" class="col-lg-4 col-md-6 col-sm-12 col-xs-12 text-center"><img class="img-circle img-responsive img-center" src="../../img/PersonalMateria/'+item.imagen+'"></img><h3>'+item.nombre+'</h3><p>Correo:'+item.correo+'</p></div>'
-					
-				);
-			});
-		})
-	}
-	if($(".page-header").text() == "Ayudantes de Deberes"){
-		$.getJSON("../../data/personal/ayudantesDeberes.json", function(items){
-			items.ayudantesDeberes.forEach(function(item){
-				$(".container .row").append(
-					
-					'<div id="ayudante" class="col-lg-4 col-md-6 col-sm-12 col-xs-12 text-center"><img class="img-circle img-responsive img-center" src="../../img/PersonalMateria/'+item.imagen+'"></img><h3>'+item.nombre+'</h3><p>Correo:'+item.correo+'</p></div>'
-					
-				);
-			});
-		})
-	}
-
-	if($(".page-header").text() == "Profesores"){
-
-		$.getJSON("../../data/personal/profesores.json", function(items){
-			console.log(items);
-			items.profesores.forEach(function(item){
-				$(".container .row").append(
-					
-					'<div id="ayudante" class="col-lg-4 col-md-6 col-sm-12 col-xs-12 text-center"><img class="img-circle img-responsive img-center" src="../../img/PersonalMateria/'+item.imagen+'"></img><h3>'+item.nombre+'</h3><p>Correo:'+item.correo+'</p><p>Oficina:'+item.Oficina+'</p><p>Paralelos:'+item.paralelos+'<p></p></div>'
-					
-				);
-			});
-		})
-	}
-	if($(".page-header").text() == "Coordinador"){
-		$.getJSON("../../data/personal/coordinador.json", function(items){
-			items.coordinador.forEach(function(item){
-				$(".container .row").append(
-					
-					'<div id="ayudante" class="col-lg-4 col-md-6 col-sm-12 col-xs-12 text-center"><img class="img-circle img-responsive img-center" src="../../img/PersonalMateria/'+item.imagen+'"></img><h3>'+item.nombre+'</h3><p>Correo:'+item.correo+'</p><p>Oficina:'+item.oficina+'</p></div>'
-					
-				);
-			});
-		})
-	}
-}
